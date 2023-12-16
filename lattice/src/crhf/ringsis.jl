dc20fa65aa96f0477bb70b2ccd628a1a19e7bdf3
qixiaoweiwei <290189613@qq.com>
2023-12-16 16:11:48 +0100

    fixed ring sis hash function

--- lattice/src/crhf/ringsis.jl
+++ lattice/src/crhf/ringsis.jl
@@ -3,6 +3,7 @@ struct HashContextRingSIS
     n::Int
     m::Int
     a::Array{Any}
+    R::Ring
     R_q::Ring  
 end
 
@@ -11,19 +12,20 @@ function HashContextRingSIS(q, n, m)
         error("Invalid paramters!")
     end
     Z_q = GF(q)
-    R_q, x = polynomial_ring(Z_q, "x")
-    R_n = residue_ring(R_q, x^n + 1)
+    R, x = polynomial_ring(Z_q, "x")
+    R_q = residue_ring(R, x^n + 1)
     a = Array{Any}(nothing, m)
     for i = 1:m
-        a[i] = rand(R_n, n-1:n-1)
+        a[i] = rand(R_q, n-1:n-1)
     end
-    return HashContextRingSIS(q, n, m, a, R_q)
+    return HashContextRingSIS(q, n, m, a, R, R_q)
 end
 
 function crhfRingSIS(z, ctx::HashContextRingSIS)
+    b = ctx.R(z)
     r = zero(ctx.R_q)
     for i = 1:ctx.m
-        r = r + z * ctx.a[i]
+        r = r + b * ctx.a[i]
     end
     return r
 end
